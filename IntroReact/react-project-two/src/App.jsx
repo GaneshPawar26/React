12dded2a8487d6cc0aa9111958f2f25a3749c35d
GaneshPawar26 <ganeshnpawar2003@gmail.com>
2026-09-26 22:39:51 +0530

    A commit

---
+++ IntroReact/react-project-two/src/App.jsx
@@ -0,0 +1,15 @@
+import UserCard from "./components/UserCard"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <UserCard name="Ganesh Pawar"/>
+      <UserCard name="Sham Shinde"/>
+      <UserCard name="Vishal Patil"/> 
+    </div>
+  )
+}
+
+export default App
